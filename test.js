9003826e54c8a23397a59559ff194b9b686ff992
Golden Dragon <arajchwald@gmail.com>
2019-03-06 23:58:21 -0600

    final changes

---
+++ test.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    var chosen;
+    var enemy;
+
+    var fighter = {
+        goku: {
+            hp: 200,
+            ap: 9,
+            cp: 12,
+            image: 'goku.png',
+        },
+        vegeta: {
+            hp: 200,
+            ap: 10,
+            cp: 11,
+            image: 'vegeta.jpg',
+        },
+        cell: {
+            hp: 300,
+            ap: 10,
+            cp: 40,
+            image: 'cell.jpg',
+        },
+        trunks: {
+            hp: 200,
+            ap: 9,
+            cp: 12,
+            image: 'Trunks.jpg_c200',
+        }
+
+    }
+    console.log(fighter.goku);
+
+
+
+})
